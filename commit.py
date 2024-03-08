930d6b319091fcee42e84167a67cda5ce1038116
duqingnian <duqingnian@163.com>
2024-03-08 18:35:30 +0800

    python auto commit at:2024-03-08 18:35:30

---
+++ commit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os
+from datetime import datetime
+
+current_time = datetime.now()
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+commit_msg = "python auto commit at:" + formatted_time
+
+os.system("git add .");
+os.system("git commit -m \"" + commit_msg +"\"");
+os.system("git push -u origin main");
+print(" ")
+print("============== GIT SYNC COMPLETE! ==============")
+
